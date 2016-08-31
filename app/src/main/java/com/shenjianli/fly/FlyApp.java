6790bf5b9695a19891e8d18add2751139a0373b1
shenjianli <cqtddt@163.com>
2016-08-31 15:54:30 +0800

    初始化项目文件

---
+++ app/src/main/java/com/shenjianli/fly/FlyApp.java
@@ -0,0 +1,101 @@
+package com.shenjianli.fly;
+
+
+import android.app.Application;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+
+import com.shenjianli.shenlib.Constants;
+import com.shenjianli.shenlib.LibApp;
+import com.shenjianli.shenlib.base.ActivityManager;
+import com.shenjianli.shenlib.exception.CrashHandler;
+import com.shenjianli.shenlib.util.FileUtils;
+import com.shenjianli.shenlib.util.LogUtils;
+
+
+/**
+ * Created by shenjianli on 2016/7/14.
+ */
+public class FlyApp extends Application {
+
+    private static FlyApp mMobileApp;
+
+    private boolean isConnect;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        LibApp.getLibInstance().setMobileContext(this);
+        if(FileUtils.getProperties(this,R.raw.mobile)){
+            String mode = FileUtils.getPropertyValueByKey("mode");
+            LogUtils.i("开发模式为：" + mode);
+            if(Constants.DEV_MODE.equals(mode)){
+                LibApp.getLibInstance().setLogEnable(true);
+                LibApp.getLibInstance().setUrlConfigManager(R.xml.url);
+            }
+        }
+        LibApp.getLibInstance().setBeanFactoryConfig(R.raw.bean);
+        //BeanFactory.getBeanFactory().initBeanFactory(R.raw.bean);
+       // BeanFactory.getBeanFactory().initBeanFactory("bean");
+
+        if(!LogUtils.isOutPutLog){
+            CrashHandler crashHandler = CrashHandler.getInstance();
+            crashHandler.init(this);
+        }
+        mMobileApp = this;
+    }
+
+    public static FlyApp getAppInstance(){
+        return  mMobileApp;
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+
+    public void setConnect(boolean b) {
+        isConnect = b;
+    }
+
+    public boolean isConnect() {
+        return isConnect;
+    }
+
+    @Override
+    public void onTrimMemory(int level) {
+        super.onTrimMemory(level);
+        switch (level) {
+            case TRIM_MEMORY_UI_HIDDEN:
+                break;
+            default:
+                break;
+        }
+    }
+
+    /**
+     * 获取当前客户端版本信息
+     */
+    public String getCurrentVersion(){
+        String versionName = "mobileVersion_";
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), 0);
+            versionName += info.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace(System.err);
+            versionName += "1.0.0";
+        }
+        versionName += "_";
+        return versionName;
+    }
+
+
+    public void exit(){
+        ActivityManager.getInstance().appExit(this);
+    }
+}
--- app/src/test/java/com/shenjianli/fly/ExampleUnitTest.java
+++ app/src/test/java/com/shenjianli/fly/ExampleUnitTest.java
@@ -2,7 +2,11 @@ package com.shenjianli.fly;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import okhttp3.HttpUrl;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
@@ -11,5 +15,51 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void test_mockServer()throws Exception{
+        // 创建一个 MockWebServer
+        MockWebServer server = new MockWebServer();
+
+        // 设置响应
+        server.enqueue(new MockResponse().setBody("hello, world!"));
+        server.enqueue(new MockResponse().setBody("sup, bra?"));
+        server.enqueue(new MockResponse().setBody("yo dog"));
+
+        // 启动服务
+        // Start the server.
+        server.start();
+
+        // 设置服务端的URL，客户端请求中使用
+        HttpUrl baseUrl = server.url("/v1/chat");
+
+        // 运行你的应用程序代码，进行HTTP请求
+        // 响应会按照上面设置中放入队列的顺序被返回
+ //       Chat chat = new Chat(baseUrl);
+//
+//        chat.loadMore();
+//        assertEquals("hello, world!", chat.message());
+//
+//        chat.loadMore();
+//        chat.loadMore();
+//        assertEquals(""
+//                + "hello, world!\n"
+//                + "sup, bra?\n"
+//                + "yo dog", chat.message());
+
+        // 可选：确认你的应用做出了正确的请求
+//        RecordedRequest request1 = server.takeRequst();
+//        assertEquals("/v1/chat/messages/", request1.getPath());
+//        assertNotNull(request1.getHeader("Authorization"));
+//
+//        RecordedRequest request2 = server.takeRequest();
+//        assertEquals("/v1/chat/message/2", request.getPath());
+//
+//        RecordedRequest request3 = server.takeRequest();
+//        assertEquals("/v1/chat/message/3", request.getPath());
+
+        // 关闭服务，因为不能重用
+        server.shutdown();
     }
 }
